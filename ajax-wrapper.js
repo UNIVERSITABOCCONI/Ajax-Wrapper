141643babb6e493b61945d39bc266a4c7ee3e0d3
Francesco Nolano <nolanofra@gmail.com>
2015-01-05 14:38:48 +0100

    Update ajax-wrapper.js
    new show and hide func

--- ajax-wrapper.js
+++ ajax-wrapper.js
@@ -1,9 +1,58 @@
-var APEXNET_ajaxWrapper_Library = APEXNET_ajaxWrapper_Library || {};
-
-/*
+var delayLoadingTimeout;
+var loadingTimeout;
+var APEXNET_Loading = {
+    /*
+    options:    {
+                    selector:
+                    delay:
+                    timeout:                
+                }
+    */
+    show: function(options) {
+        var loadingOptions = options || {};
+        var selector = loadingOptions.selector || null;
+        var delay = loadingOptions.delay || 0;
+        var timeout = loadingOptions.timeout || 0;
+        if (selector) {
+            $(selector).prop('disabled', 'true');
+            var idLoad = "apexnet_loading_" + selector.substring(1, selector.length);
+            var width = $(selector).outerWidth();
+            var height = $(selector).outerHeight();
+            $(selector).wrap("<div style='display:inline-block; position:relative; width:" + width + "px; height:" + height + "px;'></div>");
+            $(selector).after("<i id=" + idLoad + " class='fa fa-circle-o-notch fa-spin aloader'></i>");
+        } else {
+            $("body").append('<div id="apexnet_ajaxLoading" class="apexnet_ajaxLoading-cover"><div class="apexnet_ajaxLoading"><i class="fa fa-spinner fa-spin"></i></div></div>');
+            delayLoadingTimeout = setTimeout('$("#apexnet_ajaxLoading").fadeIn(200);', delay || 0);
+        }
+        if (timeout != 0) {
+            loadingTimeout = setTimeout(function(selector) {
+                APEXNET_Loading.hide(selector)
+            }, timeout);
+        }
+    },
+    hide: function(selector) {
+        if (selector) {
+            $(selector).unwrap();
+            $('#apexnet_loading_' + selector.substring(1, selector.length)).remove();
+            $(selector).removeAttr('disabled');
+        } else {
+            clearTimeout(delayLoadingTimeout);
+            $("#apexnet_ajaxLoading").fadeOut(200);
+            $("#apexnet_ajaxLoading").remove();
+        }
+        if (loadingTimeout) {
+            clearTimeout(loadingTimeout);
+            loadingTimeout = null;
+        }
+    }
+}
+var APEXNET_ajaxWrapper_Library = {
+    /*
 options: {
                 method:
+                async:
                 data:
+                traditional:
                 onBeforeSend:
                 onDone:
                 onFail:
@@ -13,69 +62,51 @@ options: {
                 delayLoading:
           }
 */
-APEXNET_ajaxWrapper_Library.ajax = (function (url, options) {
-    
-    var loading;
-    var method = options.method || "GET";
-    var data = options.data || {};
-    var onBeforeSend = options.onBeforeSend || null;
-    var onDone = options.onDone || null;
-    var onFail = options.onFail || null;
-    var onAlways = options.onAlways || null;
-    var isShowLoading = options.isShowLoading || false;
-    var selectorLoading = options.selectorLoading || null;
-    var delayLoading = options.delayLoading || 0;
-
-    $.ajax({
-        url: url,
-        type: method,
-        cache: false,
-        data: data,
-        beforeSend: function (xhr) {
-            if (isShowLoading) {
-                showLoading(selectorLoading);
+    ajax: function(url, options) {
+        var method = options.method || "GET";
+        var async = options.async || true;
+        var data = options.data || {};
+        var contentType = options.contentType || 'application/x-www-form-urlencoded; charset=UTF-8';
+        var traditional = options.traditional || false;
+        var onBeforeSend = options.onBeforeSend || null;
+        var onDone = options.onDone || null;
+        var onFail = options.onFail || null;
+        var onAlways = options.onAlways || null;
+        var isShowLoading = options.isShowLoading || false;
+        var selectorLoading = options.selectorLoading || null;
+        var delayLoading = options.delayLoading || 0;
+        $.ajax({
+            url: url,
+            type: method,
+            async: async,
+            cache: false,
+            data: data,
+            traditional: traditional,
+            contentType: contentType,
+            beforeSend: function(xhr) {
+                if (isShowLoading) {
+                    APEXNET_Loading.show({
+                        selector: selectorLoading,
+                        delay: delayLoading,
+                    });
+                }
+                if (onBeforeSend) {
+                    onBeforeSend(xhr);
+                }
             }
-            if (onBeforeSend) {
-                onBeforeSend(xhr);
-            }
-        }
-    })
-        .done(function (responseData) {
-            if (onDone)
-                onDone(responseData);
-        })
-        .fail(function (xhr, textStatus, errorThrown) {
+        }).done(function(responseData) {
+            if (onDone) onDone(responseData);
+        }).fail(function(xhr, textStatus, errorThrown) {
             if (onFail) {
                 onFail(xhr, textStatus, errorThrown);
             }
-        })
-        .always(function () {
+        }).always(function() {
             if (onAlways) {
                 onAlways();
             }
-            hideLoading(selectorLoading);
+            if (isShowLoading) {
+                APEXNET_Loading.hide(selectorLoading);
+            }
         });
-    
-
-
-function showLoading(selector) {
-    if (selector) {
-        $(selector).prop('disabled', 'true');
-        $(selector).after("<i id='apexnet_ajaxLoading' class='fa fa-spinner fa-spin'></i>");
-    } else {        
-        $("body").append('<div id="apexnet_ajaxLoading" class="apexnet_ajaxLoading-cover"><div class="apexnet_ajaxLoading"><i class="fa fa-spinner fa-spin"></i></div></div>');
-        loading = setTimeout('$("#apexnet_ajaxLoading").fadeIn(200);', delayLoading);
-    }
+    },
 }
-
-function hideLoading(selector) {
-    if (selector) {
-        $(selector).removeAttr('disabled');
-        $('#apexnet_loading').remove();
-    } else {
-        clearTimeout(loading);
-        $("#apexnet_ajaxLoading").fadeOut(200);
-        $("#apexnet_ajaxLoading").remove();
-    }
-}
-});
